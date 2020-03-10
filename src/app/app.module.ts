ae61b9e6321ac04821d44578e0b3e71272881332
Fadi AT <fadiatamny@gmail.com>
2020-03-11 01:11:23 +0200

    fully updated to angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { LogoComponent } from './branding/logo/logo.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule, MatDialogModule, MatMenuModule, MatSlideToggleModule } from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CookieService } from 'ngx-cookie-service';
-import { NgxWebstorageModule } from 'ngx-webstorage';
 import { AppComponent } from './app.component';
 import { ErrorDialogBoxComponent } from './components/error-dialog-box/error-dialog-box.component';
 import { HamburgerNavComponent } from './components/hamburger-nav/hamburger-nav.component';
@@ -36,7 +38,6 @@ import { GlobalsService } from './Services/globals.service';
         MatIconModule,
         MatButtonModule,
         MatSlideToggleModule,
-        NgxWebstorageModule.forRoot(),
         RouteModule,
     ],
     providers: [
