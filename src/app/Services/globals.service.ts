a3751259674822343ecd43ede7d1f1063ae96b8a
Fadi AT <fadiatamny@gmail.com>
2020-03-11 01:38:04 +0200

    updated dev api

--- src/app/Services/globals.service.ts
+++ src/app/Services/globals.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalsService {
     public apiKey: string = "c467fa56-5c12-4ff8-8e32-38ca6e903ea1";
-    public baseUrl: string = "https://shmoogle.azurewebsites.net";
+    // //devUrl
+    public baseUrl: string = "http://bingsearchapiv1.azurewebsites.net/";
+    //public baseUrl: string = "https://shmoogle.azurewebsites.net";
     constructor() { }
 }
