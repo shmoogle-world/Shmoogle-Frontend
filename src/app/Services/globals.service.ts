c9707b6863009b0475d91e2d4d6c21f9be01e3f2
Fadi AT <fadiatamny@gmail.com>
2020-03-23 23:04:15 +0200

    updated it back to dev url

--- src/app/Services/globals.service.ts
+++ src/app/Services/globals.service.ts
@@ -4,9 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GlobalsService {
-    public apiKey: string = "c467fa56-5c12-4ff8-8e32-38ca6e903ea1";
+    public apiKey: string = "e172c104-b919-42be-abad-dea7a2affdeb";
     // //devUrl
-    //public baseUrl: string = "http://bingsearchapiv1.azurewebsites.net/";
-    public baseUrl: string = "https://shmoogle.azurewebsites.net";
+    public baseUrl: string = "http://bingsearchapiv1.azurewebsites.net/";
+    //public baseUrl: string = "https://shmoogle.azurewebsites.net";
     constructor() { }
 }
