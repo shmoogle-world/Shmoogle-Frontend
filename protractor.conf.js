b29c2601f14a2f707f72552649b50d269c87c5f7
Elias Renman <elias.renman00@gmail.com>
2020-05-07 19:49:04 +0200

    reverted

--- protractor.conf.js
+++ protractor.conf.js
@@ -20,7 +20,7 @@ exports.config = {
     print: function() {}
   },
   onPrepare() {
-    require('ts-node').signup({
+    require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
