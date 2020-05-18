5a770b57839e1e954b1e9bba19ce4f4e73f25c7b
Elias Renman <elias.renman00@gmail.com>
2020-05-18 18:24:12 +0200

    wip

--- src/app/pages/boards/board.model.ts
+++ src/app/pages/boards/board.model.ts
@@ -2,20 +2,20 @@ export interface Board {
   id: number;
   title: string;
   description?: string;
-  views: number;
+  view_count: number;
   public: boolean;
-  saves: number;
   created_at: number | Date;
-  updated_at: number | Date;
-
+  
+  // Relation not always loaded.
   items?: BoardItem[]; 
 }
 
 export interface BoardItem {
   id: number;
   title: string;
-  preview_image: string;
-  created_at: number | Date;
-  updated_at: number | Date;
+  url: string;
+  snippet?: string;
+  preview_image?: string;
+  dateLastCrawled?: number | Date;
 
 }
