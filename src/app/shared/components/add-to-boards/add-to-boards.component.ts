21952494d26ef73307c18769aba209f0af598485
Elias Renman <elias.renman00@gmail.com>
2020-07-10 21:32:13 +0200

    finished working

--- src/app/shared/components/add-to-boards/add-to-boards.component.ts
+++ src/app/shared/components/add-to-boards/add-to-boards.component.ts
@@ -1,13 +1,14 @@
-import { User } from './../../services/auth/user.model';
-import { AuthService } from './../../services/auth/auth.service';
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
-import { Board } from '../../../pages/boards/board.model';
-import { Subscription } from 'rxjs';
-import { MatSelect } from '@angular/material/select';
-import { FormGroup } from '@angular/forms';
-import { WebResult } from '../../../pages/search-results/models/web-result.model';
-import { ImageResult } from '../../../pages/search-results/models/image-result.model';
 import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatSelect } from '@angular/material/select';
+import { Subscription } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { Board } from '../../../pages/boards/board.model';
+import { ImageResult } from '../../../pages/search-results/models/image-result.model';
+import { WebResult } from '../../../pages/search-results/models/web-result.model';
+import { AuthService } from './../../services/auth/auth.service';
+import { User } from './../../services/auth/user.model';
 
 @Component({
   selector: 'app-add-to-boards',
@@ -15,23 +16,22 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./add-to-boards.component.scss']
 })
 export class AddToBoardsComponent implements OnInit, OnDestroy {
-  
-  @Input() data: WebResult | ImageResult; 
-  
+
+  @Input() data: WebResult | ImageResult;
+
   user: null | User;
   boards: Board[];
   userSub: Subscription;
   boardSub: Subscription;
 
   addedToBoard: boolean = false;
-  sForm: FormGroup;
-  constructor(private authservice: AuthService, 
+  constructor(private authservice: AuthService,
     private http: HttpClient) { }
   ngOnDestroy(): void {
-    if(this.userSub) {
+    if (this.userSub) {
       this.userSub.unsubscribe();
     }
-    if(this.boardSub) {
+    if (this.boardSub) {
       this.boardSub.unsubscribe();
     }
   }
@@ -45,11 +45,11 @@ export class AddToBoardsComponent implements OnInit, OnDestroy {
     })
   }
 
-  onChange(e: {source: MatSelect, value: number | string} | any) {
-    
-    switch(e.value) {
-    
-      case 'new':  {
+  onChange(e: { source: MatSelect, value: number | string } | any) {
+
+    switch (e.value) {
+
+      case 'new': {
         e.source.writeValue(null);
         alert("This Function has yet to be completed");
         break;
@@ -58,17 +58,29 @@ export class AddToBoardsComponent implements OnInit, OnDestroy {
         break;
       }
       default: {
+        let payload;
+        if ((this.data as WebResult).url) {
+          this.data = <WebResult>this.data;
+          payload = {
+            title: this.data.name,
+            url: this.data.url,
+            snippet: this.data.snippet,
+            last_crawled: new Date()
+          };
+        } else {
+          this.data = <ImageResult>this.data;
+          payload = {
+            title: this.data.name,
+            url: this.data.contentUrl,
+            last_crawled: new Date()
+          }
+        }
+        console.log("this happens")
+        this.http.post(`${environment.apiEndpoint}board/${e.value}/search`, payload)
+          .subscribe(() => {
 
-        // this.http.put(`${environment.apiEndpoint}board/${e.value}`, {
-        //   items: [
-        //     {
-        //       title: this.data.name,
-        //       url: this.data.url
-        //     }
-        //   ]
-        // })
-        this.addedToBoard = true;
-        console.log("this happens", this.data)
+            this.addedToBoard = true;
+          })
       }
     }
   }
